9474585d544e746a2c45c3c61c0be7f9f254d033
GBosetti <gianmarco.bosetti@gmail.com>
2021-11-18 14:32:36 +0100

    Completato bonus

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -3,21 +3,36 @@ package org.generation.italy.cicli;
 import java.util.Scanner;
 import java.util.Random;
 
+
 public class StampaNumeriDispari {
 
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
+		Random r = new Random();
 		System.out.println("Inserisci il numero massimo: ");
 		int n = scanner.nextInt();
 		
 		int[] array = new int[n];
+		int odd = 0;
+		int even = 0;
+		int tot = 0;
+		int max = 100;
+		
 		for (int i = 0; i < n; i++) {
-			array[i] = (int) (Math.random() * 100);
+			array[i] = r.nextInt(max+1);
 			if (array[i] % 2 != 0) {
 				System.out.println("Element position " + (i+1) + ": " + array[i]);
+				odd++;
+			}else {
+				even++;
+			}
+			if (array[i] > 10 && array[i] < 50) {
+				tot += array[i];
 			}
 		}
+		
+		System.out.println("\nI numeri dispari sono: " + odd + "\nI numeri pari sono: " + even + "\nLa somma dei numeri compresi tra 10 e 50 è: " + tot);
 		
 		scanner.close();
 
